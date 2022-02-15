3e494a4d52a9bec17d8197257f8a669b6917c39d
guo-yong-qiang <2453362036@qq.com>
2022-02-16 04:54:59 +0800

    注册功能 Signed-off-by: 郭永强 <2453362036@qq.com>
    Signed-off-by: guo-yong-qiang <2453362036@qq.com>

---
+++ src/main/java/com/nowcoder/community/util/CommunityConstant.java
@@ -0,0 +1,16 @@
+package com.nowcoder.community.util;
+
+public interface CommunityConstant {
+	/**
+	 * 激活成功
+	 */
+	int ACTIVATION_SUCESS = 0;
+	/**
+	 * 重复激活
+	 */
+	int ACTIVATION_REFEAT = 1;
+	/**
+	 * 激活失败
+	 */
+	int ACTIVATION_FALLURE = 2;
+}
