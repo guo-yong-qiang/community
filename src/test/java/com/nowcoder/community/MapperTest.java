d0cc87873d22d693f4b0c200e62e4c3940c97962
guo-yong-qiang <2453362036@qq.com>
2022-02-17 07:07:13 +0800

    开发登录退出功能 Signed-off-by: 郭永强 <2453362036@qq.com>
    Signed-off-by: guo-yong-qiang <2453362036@qq.com>

--- src/test/java/com/nowcoder/community/MapperTest.java
+++ src/test/java/com/nowcoder/community/MapperTest.java
@@ -1,8 +1,10 @@
 package com.nowcoder.community;
 
 import com.nowcoder.community.dao.DiscussPostMapper;
+import com.nowcoder.community.dao.LoginTicketMapper;
 import com.nowcoder.community.dao.UserMapper;
 import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.entity.LoginTicket;
 import com.nowcoder.community.entity.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -23,6 +25,8 @@ public class MapperTest {
 
 	@Autowired
 	private DiscussPostMapper discussPostMapper;
+	@Autowired
+	private LoginTicketMapper loginTicketMapper;
 
 	@Test
 	public void testSelectUser() {
@@ -74,5 +78,23 @@ public class MapperTest {
 		System.out.println(rows);
 	}
 
+	@Test
+	public void testInsertLoginTicket() {
+		LoginTicket loginTicket = new LoginTicket();
+		loginTicket.setUserId(101);
+		loginTicket.setTicket("abc");
+		loginTicket.setStatus(0);
+		loginTicket.setExpired(new Date(System.currentTimeMillis() + 1000 * 60 * 10));
+		loginTicketMapper.insertLoginTicket(loginTicket);
+	}
 
+	@Test
+	public void testSelectLoginTiceet() {
+		LoginTicket loginTicket = loginTicketMapper.selectByTicket("abc");
+		System.out.println(loginTicket);
+
+		loginTicketMapper.updateStatus("abc", 1);
+		loginTicket = loginTicketMapper.selectByTicket("abc");
+		System.out.println(loginTicket);
+	}
 }
